35b582fca0eecab4162a8f0a96cfca864f2b72df
Aura Pasmin <55113392+auraPasmin@users.noreply.github.com>
2021-11-24 15:51:50 -0500

    repaso

---
+++ repaso-php/index.php
@@ -0,0 +1,34 @@
+<h1>Codigo html</h1>
+
+
+<?php 
+    echo "<h1>CodigoHTML en php</h1>";
+    print("hola");
+
+    #esto es un comentario;
+    //esto es otro comentario
+    /*
+    este 
+        es
+            un 
+             comentatio
+                    largo*/
+
+    echo "<!--COMENTARIO HTML-->";
+
+
+    #variables
+    $texto = "Repaso de PHP";
+    $altura = 164;
+
+    $textofinal = echo "<h1>".$texto." , mi altura es: ".$altura. "cm</h1";
+
+    echo $textofinal;
+    echo $textofinal;
+    echo $textofinal;
+
+    $textofinal = "cambiando";
+    echo $textofinal;
+
+
+?>
